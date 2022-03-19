9b8c1badf784a07f8de1709486e3c1f1c87c8ca0
Norhan Boghdadi <goodnorhan@gmail.com>
2022-03-19 15:21:31 +0200

    Add MapsView

--- MapsApplication/ViewController.swift
+++ MapsApplication/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupMap()
+
+    }
+    func setupMap() {
+        let camera = GMSCameraPosition.camera(withLatitude: 53.5499242, longitude: 9.9839786, zoom: 15.0)
+        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
+        view.addSubview(mapView)
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 53.5499242, longitude: 9.9839786)
+        marker.map = mapView
+
     }
 
 
