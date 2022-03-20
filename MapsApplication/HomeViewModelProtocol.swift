5012d832a66a053fb58a3897464689af20a8785f
Norhan Boghdadi <goodnorhan@gmail.com>
2022-03-20 14:11:12 +0200

    Add Detailsview into main view

---
+++ MapsApplication/HomeViewModelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModelProtocol.swift
+//  MapsApplication
+//
+//  Created by Norhan Boghdadi on 3/20/22.
+//
+
+import Foundation
+
+
+protocol HomeViewModelProtocol: AnyObject{
+    func longPressTap()
+}
--- MapsApplication/ViewController.swift
+++ MapsApplication/ViewController.swift
@@ -10,15 +10,19 @@ import GoogleMaps
 
 class ViewController: UIViewController {
 
+    private let locationDetailsView = LocationDetailsView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         setupMap()
+        setupLongPressor()
+        
 
     }
     //MARK: - SETUP Map & Marker initial view. 
-    func setupMap() {
+    private func setupMap() {
         let camera = GMSCameraPosition.camera(withLatitude: 53.5499242, longitude: 9.9839786, zoom: 15.0)
         let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
         view.addSubview(mapView)
@@ -29,7 +33,25 @@ class ViewController: UIViewController {
         marker.map = mapView
 
     }
+    //MARK: - Setup Details View
+    private func setupDetailsView() {
+        view.addSubview(locationDetailsView)
+        locationDetailsView.frame = CGRect(x: 20, y: (view.frame.height) - 120, width: (view.frame.width) - 40, height: 100)
+    }
+    //MARK: - Long press setup
+    private func setupLongPressor() {
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressTap))
+        longPressRecognizer.minimumPressDuration = 0.5
+        view.addGestureRecognizer(longPressRecognizer)
+    }
 
 
 }
 
+extension ViewController: HomeViewModelProtocol {
+    @objc func longPressTap() {
+        setupDetailsView()
+    }
+    
+    
+}
