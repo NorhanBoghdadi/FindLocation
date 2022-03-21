93beb24068a010178378065d7e88ff5400ba1f21
Norhan Boghdadi <goodnorhan@gmail.com>
2022-03-21 20:41:24 +0200

    Refactor HomeViewController, add ViewModel

--- MapsApplication/HomeViewModelProtocol.swift
+++ MapsApplication/HomeViewModelProtocol.swift
@@ -6,8 +6,33 @@
 //
 
 import Foundation
-
+import GooglePlaces
 
 protocol HomeViewModelProtocol: AnyObject{
-    func longPressTap()
+    var placesClient: GMSPlacesClient {get}
+    func getPlace(from placeID: String)
+}
+
+
+class HomeViewModel: HomeViewModelProtocol {
+   
+    //MARK: - Setup Places
+    var placesClient = GMSPlacesClient.shared()
+
+    func getPlace(from placeID: String) {
+        let placeFields: GMSPlaceField = [.name, .formattedAddress]
+        placesClient.fetchPlace(fromPlaceID: placeID, placeFields: placeFields, sessionToken: nil)  { (place, error) in
+            guard error == nil else {
+                print("Current place error: \(error?.localizedDescription ?? "")")
+                return
+            }
+
+            guard let place = place else {
+                print("No current place")
+                return
+            }
+            
+
+        }
+    }
 }
--- MapsApplication/ViewController.swift
+++ MapsApplication/ViewController.swift
@@ -11,90 +11,76 @@ import GooglePlaces
 
 class ViewController: UIViewController {
 
-    private let locationDetailsView = LocationDetailsView()
     private var camera = GMSCameraPosition()
     private var mapView = GMSMapView()
     private var marker = GMSMarker()
     private var placesClient = GMSPlacesClient()
-    
+    private var viewModel = HomeViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         setupMap()
         setupLongPressor()
-        setupPlaces()
         
 
     }
     //MARK: - SETUP Map & Marker initial view. 
     private func setupMap() {
-        camera = GMSCameraPosition.camera(withLatitude: 53.5499242, longitude: 9.9839786, zoom: 15.0)
+        camera = GMSCameraPosition.camera(withLatitude: Constants.initLatitude, longitude: Constants.initLongitude, zoom: Constants.cameraZoom)
         mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
         mapView.delegate = self
         view.addSubview(mapView)
         
         // Creates a marker in the center of the map.
         marker.icon = UIImage(named: "MapLocationMarker")
-        marker.position = CLLocationCoordinate2D(latitude: 53.5499242, longitude: 9.9839786)
+        marker.position = CLLocationCoordinate2D(latitude: Constants.initLatitude, longitude: Constants.initLongitude)
         marker.map = mapView
 
     }
     //MARK: - Setup Details View
     private func setupDetailsView() {
-        view.addSubview(locationDetailsView)
-        locationDetailsView.frame = CGRect(x: 20, y: (view.frame.height) - 120, width: (view.frame.width) - 40, height: 100)
+        let detailsView = LocationDetailsViewController()
+        detailsView.modalPresentationStyle = .overCurrentContext
+        present(detailsView, animated: true)
     }
+    
     //MARK: - Long press setup
     private func setupLongPressor() {
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressTap))
         longPressRecognizer.minimumPressDuration = 0.5
         view.addGestureRecognizer(longPressRecognizer)
     }
-    
-    //MARK: - Setup Places
-    private func setupPlaces() {
-        placesClient = GMSPlacesClient.shared()
-
-    }
-    private func getPlace(from placeID: String) {
-        let placeFields: GMSPlaceField = [.name, .formattedAddress]
-        placesClient.fetchPlace(fromPlaceID: placeID, placeFields: placeFields, sessionToken: nil)  { (place, error) in
-            guard error == nil else {
-                print("Current place error: \(error?.localizedDescription ?? "")")
-                return
-            }
-
-            guard let place = place else {
-                print("No current place")
-                return
-            }
-            
-            
-            print(place.name)
-            print(place.formattedAddress)
-        }
-    }
-
-}
-
-extension ViewController: HomeViewModelProtocol {
     @objc func longPressTap() {
         setupDetailsView()
     }
-    
-    
+
 }
+
 
 //MARK: - Change Map Location & Marker
 extension ViewController: GMSMapViewDelegate {
     func mapView(_ mapView: GMSMapView, didTapPOIWithPlaceID placeID: String, name: String, location: CLLocationCoordinate2D) {
+        setupDetailsView() //Remove later
         
-        getPlace(from: placeID)
-        camera = GMSCameraPosition.camera(withLatitude: location.latitude, longitude: location.longitude, zoom: 15.0)
+        viewModel.getPlace(from: placeID)
+        camera = GMSCameraPosition.camera(withLatitude: location.latitude, longitude: location.longitude, zoom: Constants.cameraZoom)
         marker.position = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
         
         print("You tapped at \(location.latitude), \(location.longitude), \(placeID)")
     }
     
+}
+extension ViewController {
+    struct Constants {
+        static let initLatitude = 53.5499242
+        static let initLongitude = 9.9839786
+        static let cameraZoom: Float = 15.0
+        
+        static let viewX: CGFloat = 20
+        static let viewY: CGFloat = 120
+        static let width: CGFloat = 40
+        static let height: CGFloat = 150
+    }
 }
